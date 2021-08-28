ce7c0ead173233f1e845c4861ee4a433c4b9b998
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-08-28 10:18:28 +0300

    Initial Commit

---
+++ Avito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 28.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
